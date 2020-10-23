ef8c4f39ed5f47c84cd2a0ffb6ebb4dc793da225
vismaya01 <kvvismaya98@gmail.com>
2020-10-23 12:26:35 +0530

    Add: Last name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,14 +2,21 @@
 echo "Wellcome to User Registration"
 
 #First name starts with Cap and has minimum 3 characters
-
-read -p "User Name : " firstName
-
+read -p "User First Name : " firstName
 pattern1='^[A-Z]{1}[a-zA-Z]{2,}$'
-
 if [[ $firstName =~ $pattern1 ]]
 then
-        echo valid
+        echo "valid First name"
 else
-        echo invalid
+        echo "invalid First name"
+fi
+
+#Last name starts with Cap and has minimum 3 characters
+read -p "User Last Name : " lastName
+pattern2='^[A-Z]{1}[a-zA-Z]{2,}$'
+if [[ $lastName =~ $pattern2 ]]
+then
+        echo "valid Last name"
+else
+        echo "invalid Last name"
 fi
