54136049d945d3434f9fb1a656534a36a9444b49
vismaya01 <kvvismaya98@gmail.com>
2020-10-23 12:12:55 +0530

    Add: First name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,15 @@
 #! /bin/bash -x
 echo "Wellcome to User Registration"
+
+#First name starts with Cap and has minimum 3 characters
+
+read -p "User Name : " firstName
+
+pattern1='^[A-Z]{1}[a-zA-Z]{2,}$'
+
+if [[ $firstName =~ $pattern1 ]]
+then
+        echo valid
+else
+        echo invalid
+fi
