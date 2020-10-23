080311d3244afa288a15e7033ea601606c097442
vismaya01 <kvvismaya98@gmail.com>
2020-10-23 14:48:27 +0530

    Modified: passowrd validation added Rule 4

--- userRegistration.sh
+++ userRegistration.sh
@@ -43,7 +43,7 @@ fi
 
 #password validation rule 1
 read -p "User Password - " password
-pattern5="^([a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$"
+pattern5="^([a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$#%*+]).{8,}$"
 if [[ $password =~ $pattern5 ]]
 then
         echo "valid password"
